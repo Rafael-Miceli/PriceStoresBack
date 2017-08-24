02e13ab30cdd23ac281503536edf052bcbf692b1
Rafael Miceli <rafael.miceli@hotmail.com>
2017-08-24 03:46:27 -0300

    Habilitando CORS

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,18 +25,25 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opt => opt.AddPolicy("CorsPolicy",
+            builder => builder.AllowAnyOrigin()));
+
             services.AddMvc();
+            services.Configure<MvcOptions>(opt => 
+            opt.Filters.Add(new CorsAuthorizationFilterFactory("CorsPolicy")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseMvc();            
         }
     }
 }
