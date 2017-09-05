42f7ea669aaa6ce191ed32efcd9d5e1a4fe7f557
Rafael Miceli <rafael.miceli@hotmail.com>
2017-09-05 18:29:43 -0300

    Relizando oprações CRUD com sucesso em container Mongo

--- ApplicationServices/ProductApplicationService.cs
+++ ApplicationServices/ProductApplicationService.cs
@@ -17,7 +17,7 @@ namespace Api.ApplicationServices
             try
             {
                 var client = new MongoClient("mongodb://192.168.99.100:27017");
-                _mongoDb = client.GetDatabase("test");
+                _mongoDb = client.GetDatabase("local");
             }
             catch (Exception ex) 
             { 
@@ -29,18 +29,19 @@ namespace Api.ApplicationServices
         public void AddProduct(Product product)
         {
             InitializeMongoDatabase(); 
-            BsonDocument doc = new BsonDocument();
-            doc.Add(new BsonElement("ProductName", product.Name));
+            //BsonDocument doc = new BsonDocument(product.ToBson());
 
-            var collection = _mongoDb.GetCollection<BsonDocument>("products");
-            collection.InsertOne(doc);
+            var collection = _mongoDb.GetCollection<Product>("products");
+            collection.InsertOne(product);
         }
 
         public Product FindByName(string productName)
         {
             Console.WriteLine($"Buscando produto {productName}");
             InitializeMongoDatabase();
-            AddProduct(new Product("Teste", 0));
+            var collection = _mongoDb.GetCollection<BsonDocument>("products");
+            Console.WriteLine(collection.ToJson());
+            //AddProduct(new Product("Teste", 0));
 
             return new Product(productName, 0);
         }
