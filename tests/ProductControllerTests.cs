374eb004b09cfd37836f463d610f02d9f96c2aa3
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-09-21 12:38:09 -0300

    Reestruturando projeto para criar testes unitarios

---
+++ tests/ProductControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class ProductControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
