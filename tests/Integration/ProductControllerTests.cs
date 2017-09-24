3af395a6f35b0c4b832ba7cec04a385302908cea
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-09-24 12:54:34 -0300

    Separando testes unitarios de integracao

---
+++ tests/Integration/ProductControllerTests.cs
@@ -0,0 +1,31 @@
+using Api.ApplicationServices;
+using Api.Data;
+using Api.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+
+namespace tests.Integration
+{
+    [TestClass]
+    public class ProductControllerTests
+    {
+        [TestMethod]
+        public async Task When_Listing_Products_Then_Return_Its_Price_And_Min_And_Max_Prices_Also()
+        {
+            var productDataMock = new Mock<IProductContext>();
+            var productService = new ProductApplicationService(productDataMock.Object);
+            var sut = new ProductController(productService);
+
+            var result = await sut.Get();   
+
+            Console.WriteLine($"Recebido {result.Count()} produtos");
+
+            Assert.IsTrue(result.Count() > 0);
+            Assert.IsTrue(result.First().MaxPrice > 0);
+            Assert.IsTrue(result.First().MinPrice > 0);
+        }
+    }
+}
--- tests/Unit/ProductControllerTests.cs
+++ tests/Unit/ProductControllerTests.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System;
 
-namespace tests
+namespace tests.Unit
 {
     [TestClass]
     public class ProductControllerTests
