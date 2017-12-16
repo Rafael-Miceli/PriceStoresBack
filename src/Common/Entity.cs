f1d3e555aaf6741bf6844f4809e898580895d515
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-12-16 10:45:10 -0200

    Adicionada classe entity

---
+++ src/Common/Entity.cs
@@ -0,0 +1,52 @@
+namespace Api.Common
+{
+    public abstract class Entity
+    {
+        public virtual string Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (ReferenceEquals(other, null))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == "" || other.Id == "")
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+
+        // private Type GetRealType()
+        // {
+        //     return NHibernateProxyHelper.GetClassWithoutInitializingProxy(this);
+        // }
+    }
+} 
