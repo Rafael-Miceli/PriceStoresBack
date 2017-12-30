9edbcfdb481524e4ef3f8cb0e4d00db6af314b8e
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-12-30 19:04:48 -0200

    Teste de chamada a fila

--- tests/Integration/NewProductQueueTests.cs
+++ tests/Integration/NewProductQueueTests.cs
@@ -6,17 +6,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using System;
 using Api.ViewModels;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace tests.Integration
 {
     [TestClass]
     public class NewProductQueueTests
     {
-        private string productQueueConnection = "";
+        private string productQueueConnection = "tcp://localhost:25672";
 
         [TestMethod]
         public async Task Smoke_Connection_To_Queue()
         {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
         }
     }
 
