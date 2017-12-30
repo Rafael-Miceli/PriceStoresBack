6f9f955d4c0fba5d50528d950237acfdd32a1f65
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-12-30 18:25:39 -0200

    Preparando conexão com fila

---
+++ tests/Integration/NewProductQueueTests.cs
@@ -0,0 +1,23 @@
+using Api.ApplicationServices;
+using Api.Data;
+using Api.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Api.ViewModels;
+
+namespace tests.Integration
+{
+    [TestClass]
+    public class NewProductQueueTests
+    {
+        private string productQueueConnection = "";
+
+        [TestMethod]
+        public async Task Smoke_Connection_To_Queue()
+        {
+        }
+    }
+
+}
