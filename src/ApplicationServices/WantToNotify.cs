2f2606c011205dee29f9daa3ce283148acb1f50e
Rafael-Miceli <rafael.miceli@hotmail.com>
2018-01-08 22:54:33 -0200

    Criando implementação e envio de notificação por fila

---
+++ src/ApplicationServices/WantToNotify.cs
@@ -0,0 +1,18 @@
+using Api.ApplicationServices.Interfaces;
+using Api.Models;
+
+namespace Api.ApplicationServices
+{
+    public class WantToNotify : IWantToNotify
+    {
+        public void NewProduct(Product message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateProduct(Product message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
