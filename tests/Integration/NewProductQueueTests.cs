70c8366195b179a13f22eeca83930718ea5dcc62
Rafael-Miceli <rafael.miceli@hotmail.com>
2018-01-12 19:52:45 -0200

    Leitura de exchange

--- tests/Integration/NewProductQueueTests.cs
+++ tests/Integration/NewProductQueueTests.cs
@@ -88,6 +88,36 @@ namespace tests.Integration
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
+
+
+            //Ler de exchange
+            //var factory = new ConnectionFactory() { HostName = "localhost" };
+            // using(var connection = factory.CreateConnection())
+            // using(var channel = connection.CreateModel())
+            // {
+            //     channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            //     var queueName = channel.QueueDeclare().QueueName;
+            //     channel.QueueBind(queue: queueName,
+            //                     exchange: "logs",
+            //                     routingKey: "");
+
+            //     Console.WriteLine(" [*] Waiting for logs.");
+
+            //     var consumer = new EventingBasicConsumer(channel);
+            //     consumer.Received += (model, ea) =>
+            //     {
+            //         var body = ea.Body;
+            //         var message = Encoding.UTF8.GetString(body);
+            //         Console.WriteLine(" [x] {0}", message);
+            //     };
+            //     channel.BasicConsume(queue: queueName,
+            //                         autoAck: true,
+            //                         consumer: consumer);
+
+            //     Console.WriteLine(" Press [enter] to exit.");
+            //     Console.ReadLine();
+            // }
         }
     }
 
