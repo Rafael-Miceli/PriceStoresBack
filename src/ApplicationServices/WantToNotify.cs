057aadadd0e505692799877605d4c3aec9bd247a
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-01-11 22:39:11 -0200

    Codigo para exchange do Rabbit

--- src/ApplicationServices/WantToNotify.cs
+++ src/ApplicationServices/WantToNotify.cs
@@ -9,25 +9,32 @@ namespace Api.ApplicationServices
     {
         public void NewProduct(Product message)
         {
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                channel.QueueDeclare(queue: "hello",
-                                durable: true,
-                                exclusive: false,
-                                autoDelete: false,
-                                arguments: null);
+            var message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "newproduct",
+                                routingKey: "",
+                                basicProperties: null,
+                                body: body);
 
-                string message = "Hello World!";
-                var body = Encoding.UTF8.GetBytes(message);
+            // var factory = new ConnectionFactory() { HostName = "localhost" };
+            // using (var connection = factory.CreateConnection())
+            // using (var channel = connection.CreateModel())
+            // {
+            //     channel.QueueDeclare(queue: "hello",
+            //                     durable: true,
+            //                     exclusive: false,
+            //                     autoDelete: false,
+            //                     arguments: null);
 
-                channel.BasicPublish(exchange: "",
-                                    routingKey: "hello",
-                                    basicProperties: null,
-                                    body: body);
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
+            //     string message = "Hello World!";
+            //     var body = Encoding.UTF8.GetBytes(message);
+
+            //     channel.BasicPublish(exchange: "",
+            //                         routingKey: "hello",
+            //                         basicProperties: null,
+            //                         body: body);
+            //     Console.WriteLine(" [x] Sent {0}", message);
+            // }
         }
 
         public void UpdateProduct(Product message)
